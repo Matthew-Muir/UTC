641d1b11396b3904ce15b61836cecdebc1c54a17
MattWellDressed <mattm@lcpud.org>
2021-06-22 16:49:13 -0700

    Base Functionality
    Able to test and see outputs for different dts.

--- UTC/Program.cs
+++ UTC/Program.cs
@@ -20,36 +20,34 @@ namespace UTC
             //****Stored Values****
             var timeStampFromXML = "2021-06-13T10:40:00";
 
-            var dstStart = "Sunday, March 14, 2021, 2:00:00 am";
-            var dstStartedNonExistent = "Sunday, March 14, 2021, 2:30:00 am";
-            var dstStartDuplicate = "Sunday, March 14, 2021, 3:00:00 am";
 
-            var dstEnd2AM01 = "Sunday, November 7, 2021, 2:00:00 am";
-            var dstEnd2AM02 = "Sunday, November 7, 2021, 2:00:00 am";
-            var dstEnd1AM01 = "Sunday, November 7, 2021, 1:00:00 am";
-            var dstEnd1AM02 = "Sunday, November 7, 2021, 1:00:00 am";
+            var boo = "Saturday, March 13, 2021, 12:00 am"; // dst should be false
+            var dstExpired = "Sunday, March 14, 2021, 12:00 am";
+            var foo = "Monday, March 15, 2021, 12:00 am";  // dst should be true
+            var doo = "Tuesday, June 22, 2021, 12:00 am"; //dst should be true
+            var pstStart = "Sunday November 7, 2021, 12:00 am";
+            var dstStart = "Sunday, March 13, 2022, 12:00 am";
 
             //****Creation of Objects****
 
-            var currentLocalDTS = DateTime.Now;
-            var currentUtcDTS = DateTime.UtcNow;
+            var dtsTest = DateTime.Parse(dstExpired);
+            dtsTest = DateTime.SpecifyKind(dtsTest, DateTimeKind.Local);
 
-            Console.WriteLine($"Local Time: {currentLocalDTS}");
-            Console.WriteLine($" UTC  Time: {currentUtcDTS}");
+            TimePeriodCheck(dtsTest);
+        }
 
+        public static void TimePeriodCheck(DateTime dateTime)
+        {
+            var utcDTS = dateTime.ToUniversalTime();
+            Console.WriteLine($"");
 
-
-
-
-
-
-
-
-
-
-
-
-
+            for (int i = 0; i <= 1440; i += 60)
+            {
+                
+                Console.WriteLine($"PST: {dateTime.AddMinutes(i)} - UTC {utcDTS.AddMinutes(i)} - K: {dateTime.AddMinutes(i).Kind} - DST: {dateTime.AddMinutes(i).IsDaylightSavingTime()}");
+                
+            }
+            
         }
     }
 }
